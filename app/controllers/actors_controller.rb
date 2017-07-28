37e0df8f175afe3a418bf4fec2fbf3cc40c5613a
Felipe Estrada <felipe.estradagomez@gmail.com>
2017-07-27 20:29:27 -0500

    asd

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -9,4 +9,39 @@ class ActorsController < ApplicationController
 		@actor = Actor.new
 	end
 
+
+	def create
+		@actor = Actor.new(actor_params)
+		if @actor.save
+			redirect_to actors_path
+		else
+			render actors_new_path
+		end
+	end
+
+	private 
+		   # Use callbacks to share common setup or constraints between actions.
+    def set_actor
+      @actor = Actor.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def actor_params
+      params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date,:death_place)
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
